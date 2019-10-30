67b21bf586d592af84ba6bcd5146b15b0cc4516c
leoor101 <leo260999@gmail.com>
2019-10-30 10:11:22 -0500

    base

---
+++ src/main/java/pe/edu/upc/demo/TfWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfWebApplication.class, args);
+	}
+
+}
