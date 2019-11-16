865499c4325dddf11dbe203b955c97fcbf39e238
ArmandoSL <sotolinaresa@gmail.com>
2019-11-16 02:21:24 -0500

    Login Registrar usuario completo

--- src/main/java/pe/edu/upc/controller/UsersController.java
+++ src/main/java/pe/edu/upc/controller/UsersController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,41 +23,42 @@ public class UsersController {
 
 	@Autowired
 	private UsersServiceImpl uS;
-	//@Autowired
-	//private private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	@Secured("ROLE_USER")
 	@GetMapping("/new")
 	public String newUsers(Model model)
 	{
-		model.addAttribute("users",new Users());
+		model.addAttribute("user",new Users());
 		return "user/user";
 	}
 	@PostMapping("/save")
 	public String saveCustomer(@Valid Users user_, BindingResult result, Model model, SessionStatus status)
 			throws Exception {
 		if (result.hasErrors()) {
-			return "users/users";
+			return "accounting/listAccounting";
 		} else {
 			user_.setEnabled(true);
-			uS.insert(user_);
+			
 			
 			Users aux = new Users();
 			aux.setUsername(user_.getUsername());
 			aux.setPassword(user_.getPassword());
 			aux.setEnabled(true);
 
-		//	String bcryptPassword = passwordEncoder.encode(aux.getPassword());
-		//	aux.setPassword(bcryptPassword);
+			String bcryptPassword = passwordEncoder.encode(aux.getPassword());
+			user_.setPassword(bcryptPassword);
+			uS.insert(user_);
 		
-			//aux = uS.BuscarPorNombre(user_.getUsername());
+			aux = uS.BuscarPorNombre(user_.getUsername());
 
 			uS.insRol("ROLE_USER", aux.getUserID());
 		
 			if (uS.insert(user_)==0) {
-				return "redirect:/customers/list";
+				return "redirect:/accountings/new";
 			} else {
 				model.addAttribute("mensaje", "Ocurrió un error");
-				return "redirect:/customers/new";
+				return "redirect:/accountings/list";
 			}
 		}
 	}
